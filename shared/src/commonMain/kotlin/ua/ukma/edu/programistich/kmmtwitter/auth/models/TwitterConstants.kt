6d0cb1194f6e74779562cfe321bc287cabc262e3
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2023-01-18 22:24:22 +0200

    shared gitignore

---
+++ shared/src/commonMain/kotlin/ua/ukma/edu/programistich/kmmtwitter/auth/models/TwitterConstants.kt
@@ -0,0 +1,7 @@
+package ua.ukma.edu.programistich.kmmtwitter.auth.models
+
+object TwitterConstants {
+    const val consumerApiKey = "AifBYBIYAzUCOGiEfXU0bAT3U"
+    const val consumerApiSecretKey = "jXEzvMnxS5bxiJNkrtHnTMFL2crfLvcH6NB8ZrqBlAOceA1hKN"
+    const val callback = "twittersdk"
+}
